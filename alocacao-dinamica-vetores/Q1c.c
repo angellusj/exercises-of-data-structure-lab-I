49e586ad3f0b2f77db334927096135a0888e12e9
angellusj <123104907+angellusj@users.noreply.github.com>
2023-02-24 08:22:42 -0300

    Update Q1c.c

--- alocacao-dinamica-vetores/Q1c.c
+++ alocacao-dinamica-vetores/Q1c.c
@@ -4,26 +4,17 @@
 
 #define maxchar 100
 
-// mulheres que gostaram
 float obterPorcentagemA(int qnt_entrevistado, char **sexo, char **opiniao) {
   float porcentagem = 0;
-  /*
-          \n ao final, pois foi obtido através do fgets
-          o operador ternário iria se questionar se o sexo é feminino
-          caso seja, ele irá também se questionar se a opinião é "gostou"
-          e então ou irá aumentar o contador ou irá se manter o mesmo.
-  */
   for (size_t i = 0; i < qnt_entrevistado; i++) {
     porcentagem = (strcmp(sexo[i], "feminino\n") == 0 &&
                    strcmp(opiniao[i], "gostou\n") == 0)
                       ? porcentagem + 1
                       : porcentagem;
   }
-  return (float)(porcentagem * 100 /
-                 qnt_entrevistado); // calcula a porcentagem na saída/retorno.
+  return (float)(porcentagem * 100 /qnt_entrevistado);
 }
 
-// homens que não gostaram
 float obterPorcentagemB(int qnt_entrevistado, char **sexo, char **opiniao) {
   float porcentagem = 0;
   for (size_t i = 0; i < qnt_entrevistado; i++) {
@@ -32,8 +23,7 @@ float obterPorcentagemB(int qnt_entrevistado, char **sexo, char **opiniao) {
                       ? porcentagem + 1
                       : porcentagem;
   }
-  return (float)(porcentagem * 100 /
-                 qnt_entrevistado); // calcula a porcentagem na saída/retorno.
+  return (float)(porcentagem * 100 /qnt_entrevistado); 
 }
 
 int main(void) {
